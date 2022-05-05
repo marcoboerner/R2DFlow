f676307cd1fe8b0bae55e6d6c8454bb0dffa045e
Marco Boerner <mail@marcoboerner.com>
2022-05-05 23:37:52 +0200

    Refactoring

--- Example/R2DFlow/AppDelegate.swift
+++ Example/R2DFlow/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
-        print(UserDefaults.standard.dictionaryRepresentation())
+        // print(UserDefaults.standard.dictionaryRepresentation())
 
         // Override point for customization after application launch.
         return true
